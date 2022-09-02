b6cd1f7fcc39d31303665d3ee8398ba39b951a9c
Alessandro Lollo <alessandro.lollo@cloudacademy.com>
2022-09-02 14:20:08 +0200

    Refactor soda_configuration block

--- prefect_soda_core/soda_configuration.py
+++ prefect_soda_core/soda_configuration.py
@@ -3,6 +3,8 @@ from typing import Optional
 
 from prefect.blocks.core import Block
 from pydantic import HttpUrl, root_validator
+from yaml import safe_dump, safe_load
+from yaml.error import YAMLError
 
 from prefect_soda_core.exceptions import SodaConfigurationException
 
@@ -15,8 +17,7 @@ class SodaConfiguration(Block):
     [official docs](https://docs.soda.io/soda-core/configuration.html#configuration-instructions)  # noqa
     """
 
-    configuration_yaml_file: Optional[str]
-    configuration_yaml_files: Optional[str]
+    configuration_yaml_path: str
     configuration_yaml_str: Optional[str]
 
     _block_type_name: Optional[str] = "Soda Configuration"
@@ -25,18 +26,28 @@ class SodaConfiguration(Block):
     @root_validator(pre=True)
     def check_block_configuration(cls, values):
         """
-        Ensure that at least one configuration option is passed
+        Ensure that the configuration options are valid
         """
-        configuration_yaml_file_exists = bool(values.get("configuration_yaml_file"))
-        configuration_yaml_files_exists = bool(values.get("configuration_yaml_files"))
         configuration_yaml_str_exists = bool(values.get("configuration_yaml_str"))
 
-        if not (
-            configuration_yaml_file_exists
-            or configuration_yaml_files_exists
-            or configuration_yaml_str_exists
-        ):
-            msg = "Please provide the path to either a configuration file, a configuration folder, or a configuration YAML string."  # noqa
-            raise SodaConfigurationException(msg)
+        # If the YAML string is passed, but is not a valid YAML, then raise error
+        if configuration_yaml_str_exists:
+            try:
+                yaml_str = values.get("configuration_yaml_str")
+                safe_load(yaml_str)
+            except YAMLError as exc:
+                msg = f"The provided configuration YAML is not valid. Error is: {exc}"
+                raise SodaConfigurationException(msg)
 
         return values
+
+    def persist_configuration(self):
+        """
+        Persist Soda configuration on the file system, if necessary.
+        Please note that, if the path already exists, it will be overwritten
+        """
+
+        # If a YAML string and path are passed, then persist the configuration
+        if self.configuration_yaml_str and self.configuration_yaml_path:
+            with open(self.configuration_yaml_path, "w") as f:
+                safe_dump(data=self.configuration_yaml_str, stream=f)
