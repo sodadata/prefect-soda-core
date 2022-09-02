4ab4df71089301be6163e11116ea779caa59eb44
Alessandro Lollo <alessandro.lollo@cloudacademy.com>
2022-09-02 14:20:22 +0200

    Refactor soda_configuration block tests

--- tests/test_soda_configuration.py
+++ tests/test_soda_configuration.py
@@ -4,23 +4,40 @@ from prefect_soda_core.exceptions import SodaConfigurationException
 from prefect_soda_core.soda_configuration import SodaConfiguration
 
 
-def test_soda_configuration_construction_raises():
-    msg_match = "Please provide the path to either a configuration file, a configuration folder, or a configuration YAML string."  # noqa
+def test_soda_configuration_construction_with_invalid_yaml_raises():
+    msg_match = "The provided configuration YAML is not valid."
     with pytest.raises(SodaConfigurationException, match=msg_match):
         SodaConfiguration(
-            configuration_yaml_file=None,
-            configuration_yaml_files=None,
-            configuration_yaml_str=None,
+            configuration_yaml_path="/path/to/configuration.yaml",
+            configuration_yaml_str="""
+            root:
+                - a_list
+                a_node
+            """,
         )
 
 
-def test_soda_configuration_construction_with_yaml_file_succeed():
-    yaml_file_path = "/path/to/configuration.yaml"
-
-    scc = SodaConfiguration(
-        configuration_yaml_file=yaml_file_path,
-        configuration_yaml_files=None,
-        configuration_yaml_str=None,
+def test_soda_configuration_construction_without_yaml_succeed():
+    expected_path = "/path/to/configuration.yaml"
+    sc = SodaConfiguration(
+        configuration_yaml_path=expected_path, configuration_yaml_str=None
     )
 
-    assert scc.configuration_yaml_file == yaml_file_path
+    assert sc.configuration_yaml_path == expected_path
+    assert sc.configuration_yaml_str is None
+
+
+def test_soda_configuration_construction_with_valid_yaml_succeed():
+    expected_path = "/path/to/configuration.yaml"
+    expected_yaml = """
+    root:
+        a_list:
+            key1: val1
+            key2: val2
+    """
+    sc = SodaConfiguration(
+        configuration_yaml_path=expected_path, configuration_yaml_str=expected_yaml
+    )
+
+    assert sc.configuration_yaml_path == expected_path
+    assert sc.configuration_yaml_str == expected_yaml
